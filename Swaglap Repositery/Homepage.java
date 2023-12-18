0165bf9c17f655edbcb89df3e7a624c559ba35bd
Omkar Kakade <148191564+Omkar2472@users.noreply.github.com>
2023-12-18 11:26:52 +0530

    Create Homepage.java

---
+++ Swaglap Repositery/Homepage.java
@@ -0,0 +1,9 @@
+package SwaglapRepositery;
+
+import org.openqa.selenium.By;
+
+public class homepage {
+	public By menu = By.id("react-burger-menu-btn");
+	public By logout = By.id("logout_sidebar_link");
+	public By backpack = By.xpath("//*[@id='item_4_title_link']/div");
+}

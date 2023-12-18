4c7f45d7cebe4d31a7b6d6fe96caf0082c9c3173
Omkar Kakade <148191564+Omkar2472@users.noreply.github.com>
2023-12-18 11:29:55 +0530

    Create productdetailspage.java

---
+++ Swaglap Repositery/productdetailspage.java
@@ -0,0 +1,12 @@
+package SwaglapRepositery;
+
+import org.openqa.selenium.By;
+
+public class productdetailspage {
+
+	 public By productname = By.xpath("//*[@id='inventory_item_container']/div/div/div[2]/div[1]");
+	 public By productdesc = By.xpath("//div[@class='inventory_details_desc large_size']");
+	 public By productcost = By.xpath("//div[@class='inventory_details_price']");
+	 public By backtoproductsbutton = By.id("back-to-products");
+	
+} 
